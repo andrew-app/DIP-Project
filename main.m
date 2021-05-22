12ba16476b3e8e3c337725c69833b013f5fd03a1
cardhock <monkeyxuan@Hotmail.com>
2021-05-22 21:54:56 +1000

    Update main.m

--- main.m
+++ main.m
@@ -1,4 +1,23 @@
-img = imread('images/pills1.png');
+% Load test image (coin) 
+imgtest = imread('coins.jpg');
+imgA = double(imgtest)./255;
+img1 = rgb2gray(imgA);
+imgBin = imbinarize(img1);
 figure(1)
-imshow(img);
+imshow(imgBin);
+
+% Convert objects to white
+BinImg = imcomplement(imgBin);
+figure(2)
+imshow(BinImg);
+
+% Get rid of background
+se = strel('square',4);
+BinImgA = imopen(BinImg,se);
+BinImgB = imclose(BinImgA,se);
+
+figure(3)
+imshow(BinImgB);
+[labelImg, numOfObj] = bwlabel(BinImgB);
+measurement = regionprops(labelImg);
 
