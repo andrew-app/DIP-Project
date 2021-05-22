fa4c7b582c15ece5c03f5894b17fd87ed81ae4cf
andrewapp <andrew.appuhamy@gmail.com>
2021-05-22 21:21:44 +1000

    circle outlines

--- main.m
+++ main.m
@@ -2,6 +2,20 @@ img = imread('images/pills1.png');
 
 imb = imbinarize(img);
 
+se = strel('disk', 12,0);
+
+imbe = imerode(imb,se);
 
 figure(1)
-imshow(imb)
+imshow(imbe)
+stats = regionprops('table',imbe,'Centroid',...
+    'MajorAxisLength','MinorAxisLength');
+
+centers = stats.Centroid;
+diameters = mean([stats.MajorAxisLength stats.MinorAxisLength],2);
+radii = diameters/2;
+
+figure(1)
+hold on
+viscircles(centers,radii);
+hold off
