370e0d32ebe6b4af5f4ff4298fb0415d8c2a4d18
andrewapp <andrew.appuhamy@gmail.com>
2021-05-26 12:50:41 +1000

    radius iteration

--- edge_detect.m
+++ edge_detect.m
@@ -14,59 +14,72 @@ bw = imbinarize(blur);
 
 bwe = edge(bw,'Canny');
 
-radius = str2double(coins(1,2));
+radius = 14;
 
-diameter = radius*2;
+
 
 figure(1)
 imshow(bwe)
 
-
-toc = 0;
-T = 0.35; %pixel number threshold
+coin = zeros(2,16);
 
 
+for a = 1:16
+    radius = 9 + a;
+    toc = 0;
+
+    for i = radius:radius:size(bwe,1)
+        for j = radius:radius:size(bwe,2)
+            myImage = zeros(size(bwe,1), size(bwe,2),'logical');
+            xCenter = j;
+            yCenter = i;
+
+            theta = linspace(0, 2*pi, round(4 * pi * radius)); 
+
+            x = radius * cos(theta) + xCenter;
+            y = radius * sin(theta) + yCenter;
 
 
-for i = radius:radius:size(bwe,1)
-    for j = radius:radius:size(bwe,2)
-        myImage = zeros(size(bwe,1), size(bwe,2),'logical');
-        xCenter = j;
-        yCenter = i;
 
-        theta = linspace(0, 2*pi, round(4 * pi * radius)); 
-
-        x = radius * cos(theta) + xCenter;
-        y = radius * sin(theta) + yCenter;
-
-
-
-        for k = 1:length(x)
-            row = round(y(1,k));
-            col = round(x(1,k));
-            if col == 0
-                col = 1;
-            end
-    
-            if row == 0
-                row = 1; 
-            end
-            myImage(row,col) = 1;
-            cmp = zeros(size(bwe,1), size(bwe,2),'logical');
-            for l = 1:size(bwe,1)
-                for m = 1:size(bwe,2)
-                    cmp(l,m) = bitand(bwe(l,m),myImage(l,m));
-                    
+            for k = 1:length(x)
+                row = round(y(1,k));
+                col = round(x(1,k));
+                if col == 0
+                    col = 1;
                 end
-            end
+    
+                if row == 0
+                    row = 1; 
+                end
+                myImage(row,col) = 1;
+                cmp = zeros(size(bwe,1), size(bwe,2),'logical');
+                for l = 1:size(bwe,1)
+                    for m = 1:size(bwe,2)
+                        cmp(l,m) = bitand(bwe(l,m),myImage(l,m));
+                    
+                    end
+                end
             
-            total = nnz(cmp);
-            if total >= 14
-                toc = toc + 1;
-            end
+                total = nnz(cmp);
+                if total >= 15
+                    toc = toc + 1;
+                end
+                
+                if toc == 6
+                    coin(1,a) = radius;
+                    coin(2,a) = toc;
+                    return
+                end
             
+            end
         end
     end
+    
+    coin(1,a) = radius;
+    coin(2,a) = toc;
+    
+     
 end
+
 figure(2)
 imshow(myImage);
